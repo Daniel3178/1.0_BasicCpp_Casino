427cb4b4f71278b46f6225621d95ec12a73ad668
Daniel3178 <89932502+Daniel3178@users.noreply.github.com>
2023-04-17 22:26:01 +0200

    Added some functions

--- 1.0_BasicCpp_Casino_1.1/1.0_BasicCpp_Casino_1.1.cpp
+++ 1.0_BasicCpp_Casino_1.1/1.0_BasicCpp_Casino_1.1.cpp
@@ -8,21 +8,40 @@ void DisplayRules() {
 	std::cout << rules << std::endl;
 }
 
+int GetRightFormatInt() {
+	int usrInput{ 0 };
+	while (!(std::cin >> usrInput)) {
+		std::cout << "Wrong answer format, please try again ... \n	";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+	return usrInput;
+}
+
+char GetRightFormatChar() {
+	char usrInput{ 0 };
+	while (!(std::cin >> usrInput)) {
+		std::cout << "Wrong answer format, please try again ...";
+		std::cin.clear();
+	}
+	return usrInput;
+}
+
 int GetUserInput() {
-	int userInput{ 0 };
-	bool wrong = false;
+	bool wrong = true;
 	system("cls");
 	std::cout << "Guess a number between 2 and 12 : ";
+	int userInput = GetRightFormatInt();
 	while (userInput < 2 || userInput >12) {
-		if (wrong) {
+		system("cls");
+		std::cout << "Guess a number between 2 and 12 : ";
+		if (wrong == true) {
 			system("cls");
 			std::cout << "please follow the instruction! \n";
 			std::cout << "Guess a number between 2 and 12 : ";
 		}
-		std::cin >> userInput;
-		if (userInput < 2 || userInput >12) {
-			wrong = true;
-		}
+		userInput = GetRightFormatInt();
+
 	}
 	system("cls");
 	std::cout << "	Let's see how lucky you are!";
@@ -46,19 +65,39 @@ void GetDieSum() {
 
 int main()
 {
-	int a = GetUserInput();
-	std::cout << "\n	Your choise is : " << a;
-	GetDieSum();
-	std::cout << "\n	The sum is : " << sum;
+	bool isGameActiv{ true };
+	while (isGameActiv) {
+		system("cls");
+		int a = GetUserInput();
+		char userChoice{ 'n' };
+		std::cout << "\n	Your choise is : " << a;
+		GetDieSum();
+		std::cout << "\n	The sum is : " << sum;
 
-	if (sum == a) {
-		std::cout << "\n	congratulation! You won!";
+		if (sum == a) {
+			std::cout << "\n	congratulation! You won!";
+		}
+		else {
+			std::cout << "\n	Sorry, you lost!";
+
+		}
+
+		std::cout << "\n	Do you want to try again [y / n]: ";
+		userChoice = GetRightFormatChar();
+		while (userChoice != 'n' && userChoice != 'y') {
+			system("cls");
+			std::cout << "\n	invalid input try again, [y/n]: ";
+			userChoice = GetRightFormatChar();
+			if (userChoice == 'n') {
+				isGameActiv = false;
+			}
+
+		}
+		if (userChoice == 'n') {
+			isGameActiv = false;
+		}
 	}
-	else {
-		std::cout << "\n	Sorry, you lost!";
-		std::cout << "\n	Sorry, you lost GitTest!!";
-	}
-	
+	return 0;
 }
 
 
